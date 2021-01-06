0722458a354227150357ea0d21ae562de115724f
wolfhard.fehre <wolfhard.fehre@motion-tag.com>
2021-01-06 20:14:41 +0100

    set up test for user model

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -5,12 +5,9 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../../config/config.json')[env];
 
-
-const db = {
-  User: require("./user")
-};
+const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
@@ -38,4 +35,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
