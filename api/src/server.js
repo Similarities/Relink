728bb244aef53620fce6be7f70961f178d3d084e
Nice Fontaine <wolfhard.fehre@motion-tag.com>
2021-02-06 17:02:52 +0100

    Add user seeds (#20)
    * add user seeds 
    * get all users endpoint
    * get user by id endpoint

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,29 @@
-const express = require("express");
+import express from "express";
+import db from "./models/index";
+
 const app = express();
-const db = require("./models");
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/', (_, res) => {
+    res.json({ 'message': 'welcome to the relink api' })
+});
+
+app.get('/users', async (_, res) => {
+    const users = await db.User.findAll({
+        attributes: ['id', 'name', 'email', 'shortDescription', 'contactable']
+    });
+    res.json({ 'users': users });
+})
+
+app.get('/users/:id', async (req, res) => {
+    const user = await db.User.findByPk(req.params.id, {
+        attributes: ['id', 'name', 'email', 'shortDescription', 'contactable']
+    });
+    res.json({ 'user': user });
+})
 
 db.sequelize
     .sync()
