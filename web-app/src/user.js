bd296d40239bf6f103edb7d779d7c1dbb96795bd
similarities <j.braenzel@gmx.net>
2021-03-17 22:47:32 +0100

    user co. created via array function

--- web-app/src/user.js
+++ web-app/src/user.js
@@ -1,11 +1,15 @@
 class User {
-  constructor(meta, parent, canvas, size, headerHeight) {
+  constructor(meta, parent, canvas, size, headerHeight, footerHeight, gridmap) {
     this.meta = meta;
     this.parent = parent;
     this.canvas = canvas;
     this.size = size;
     this.headerHeight = headerHeight;
-    this.position = this.startPosition();
+    this.footerHeight = footerHeight;
+    console.log(this.footerHeight);
+    this.gridmap = gridmap;
+    this.position = this.gridmap.generateCoordinates();
+    //console.log(this.position, 'coordinates')
     this.velocity = new Position(10, 10);
     this.element = this.div();
   }
@@ -14,6 +18,11 @@ class User {
     const x = Math.round(Math.random() * this.canvas.width);
     const y = Math.round(Math.random() * (this.canvas.height - this.size - 2)) + this.headerHeight;
     return new Position(x, y);
+  }
+
+
+  overflow(){
+  
   }
 
   div() {
@@ -27,19 +36,24 @@ class User {
 
     const element = document.createElement("div");
     element.className = "user";
+    if (this.position.y > this.headerHeight && this.position.y < this.footerHeight ){
+
     element.style.position = "absolute";
     element.style.width = `${this.size}px`;
     element.style.height = `${this.size}px`;
     element.style.left = `${this.position.x}px`;
     element.style.top = `${this.position.y}px`;
-
     element.appendChild(name);
     element.appendChild(shortDescription);
     this.parent.appendChild(element);
     return element;
+    }
+    else{console.log('neither')};
+
   }
 
   update() {
+    //toDo move until position is not having overlapp
     this.position = this.position.add(this.velocity);
     this.element.style.left = `${this.position.x}px`;
     this.element.style.top = `${this.position.y}px`;
